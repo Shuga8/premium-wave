0e908fbc5c85b99fac7001613ab4fc9836c019ab
Shuga8 <lotocharles8@gmail.com>
2024-05-11 17:04:55 +0100

    persis data

--- core/resources/views/admin/layouts/master.blade.php
+++ core/resources/views/admin/layouts/master.blade.php
@@ -2,76 +2,76 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $general->siteName($pageTitle ?? '') }}</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>{{ config('app.name') . ' - ' . $pageTitle }}</title>
 
-    <link rel="shortcut icon" type="image/png" href="{{ siteFavicon() }}">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('assets/global/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/admin/css/vendor/bootstrap-toggle.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/global/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/global/css/line-awesome.min.css') }}">
+	<link type="image/png" href="{{ asset('assets/global/images/logo.png') }}" rel="shortcut icon">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	<link href="{{ asset('assets/global/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/admin/css/vendor/bootstrap-toggle.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/global/css/all.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/global/css/line-awesome.min.css') }}" rel="stylesheet">
 
-    @stack('style-lib')
+	@stack('style-lib')
 
-    <link rel="stylesheet" href="{{ asset('assets/global/css/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/admin/css/app.css') }}">
+	<link href="{{ asset('assets/global/css/select2.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/admin/css/app.css') }}" rel="stylesheet">
 
-    @stack('style')
+	@stack('style')
 </head>
 
 <body>
 
-    @yield('content')
+	@yield('content')
 
-    <script src="{{ asset('assets/global/js/jquery-3.6.0.min.js') }}"></script>
-    <script src="{{ asset('assets/global/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/admin/js/vendor/bootstrap-toggle.min.js') }}"></script>
-    <script src="{{ asset('assets/admin/js/vendor/jquery.slimscroll.min.js') }}"></script>
+	<script src="{{ asset('assets/global/js/jquery-3.6.0.min.js') }}"></script>
+	<script src="{{ asset('assets/global/js/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ asset('assets/admin/js/vendor/bootstrap-toggle.min.js') }}"></script>
+	<script src="{{ asset('assets/admin/js/vendor/jquery.slimscroll.min.js') }}"></script>
 
 
-    <script>
-        window.my_pusher = {
-            'app_key'    : "{{ base64_encode(config('app.PUSHER_APP_KEY')) }}",
-            'app_cluster': "{{ base64_encode(config('app.PUSHER_APP_CLUSTER')) }}",
-            'base_url'   : "{{ route('home') }}"
-        }
-        window.allow_decimal="{{ $general->allow_decimal_after_number }}";
-    </script>
-    
-    @include('partials.notify')
-    @stack('script-lib')
-    <script>
-        window.allow_decimal = "{{ $general->allow_decimal_after_number }}";
-    </script>
+	<script>
+		window.my_pusher = {
+			'app_key': "{{ base64_encode(config('app.PUSHER_APP_KEY')) }}",
+			'app_cluster': "{{ base64_encode(config('app.PUSHER_APP_CLUSTER')) }}",
+			'base_url': "{{ route('home') }}"
+		}
+		window.allow_decimal = "{{ $general->allow_decimal_after_number }}";
+	</script>
 
-    <script src="{{ asset('assets/global/js/nicEdit.js') }}"></script>
+	@include('partials.notify')
+	@stack('script-lib')
+	<script>
+		window.allow_decimal = "{{ $general->allow_decimal_after_number }}";
+	</script>
 
-    <script src="{{ asset('assets/global/js/select2.min.js') }}"></script>
-    <script src="{{ asset('assets/admin/js/app.js') }}"></script>
+	<script src="{{ asset('assets/global/js/nicEdit.js') }}"></script>
 
-    {{-- LOAD NIC EDIT --}}
-    <script>
-        "use strict";
-        bkLib.onDomLoaded(function() {
-            $(".nicEdit").each(function(index) {
-                $(this).attr("id", "nicEditor" + index);
-                new nicEditor({
-                    fullPanel: true
-                }).panelInstance('nicEditor' + index, {
-                    hasPanel: true
-                });
-            });
-        });
-        (function($) {
-            $(document).on('mouseover ', '.nicEdit-main,.nicEdit-panelContain', function() {
-                $('.nicEdit-main').focus();
-            });
-        })(jQuery);
-    </script>
+	<script src="{{ asset('assets/global/js/select2.min.js') }}"></script>
+	<script src="{{ asset('assets/admin/js/app.js') }}"></script>
 
-    @stack('script')
+	{{-- LOAD NIC EDIT --}}
+	<script>
+		"use strict";
+		bkLib.onDomLoaded(function() {
+			$(".nicEdit").each(function(index) {
+				$(this).attr("id", "nicEditor" + index);
+				new nicEditor({
+					fullPanel: true
+				}).panelInstance('nicEditor' + index, {
+					hasPanel: true
+				});
+			});
+		});
+		(function($) {
+			$(document).on('mouseover ', '.nicEdit-main,.nicEdit-panelContain', function() {
+				$('.nicEdit-main').focus();
+			});
+		})(jQuery);
+	</script>
+
+	@stack('script')
 </body>
 
 </html>
